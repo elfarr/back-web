9502b0f5a1ebed16814e99af64d53a28fb2583c9
greta novak <114183568+elfarr@users.noreply.github.com>
2024-04-16 10:36:06 +0300

    Update logout.php

--- 5/logout.php
+++ 5/logout.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
 session_destroy();
-setcookie('fio', '', 100000);
-setcookie('bio', '', 100000);
-setcookie('gen', '', 100000);
-setcookie('date', '', 100000);
-setcookie('email', '', 100000);
-setcookie('tel', '', 100000);
+setcookie('fio_value', '', 100000);
+setcookie('bio_value', '', 100000);
+setcookie('gen_value', '', 100000);
+setcookie('date_value', '', 100000);
+setcookie('email_value', '', 100000);
+setcookie('tel_value', '', 100000);
 setcookie('languages', '', 100000);
 header("Location: index.php"); // Перенаправление на страницу form.php после выхода
 exit();
