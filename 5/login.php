963f07a1d00f26fcdb16be5ea5f1e7465a2ec808
greta novak <114183568+elfarr@users.noreply.github.com>
2024-04-17 21:56:18 +0300

    Update login.php

--- 5/login.php
+++ 5/login.php
@@ -52,7 +52,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 }
 else {
   include '../4/p.php';
-
+  echo('мы тут');
   $db = new PDO('mysql:host=127.0.0.1;dbname=u67314', $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
