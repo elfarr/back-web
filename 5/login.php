a2e8325b1c16fc5b6afdf03a82f3d23b23ab2d5b
greta novak <114183568+elfarr@users.noreply.github.com>
2024-04-17 21:49:06 +0300

    Update login.php

--- 5/login.php
+++ 5/login.php
@@ -6,6 +6,14 @@ header('Content-Type: text/html; charset=UTF-8');
 $session_started = false;
 if ($_COOKIE[session_name()] && session_start()) {
   $session_started = true;
+  if (!empty($_SESSION['login'])) {
+    // Если есть логин в сессии, то пользователь уже авторизован.
+    // TODO: Сделать выход (окончание сессии вызовом session_destroy()
+    //при нажатии на кнопку Выход).
+    // Делаем перенаправление на форму.
+    header('Location: ./');
+    exit();
+  }
 }
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   ?>
@@ -63,10 +71,8 @@ if($pas['pass'] != $_POST['pass']) {
     print($_POST['pass']);
     exit("Неверный пароль");
 }
-  else{
     if (!$session_started) {
       session_start();
-    }
       $_SESSION['login'] =  $logLogin;
       $_SESSION['pass'] =  $passLogin;
       $_SESSION['uid'] = $pas['id'];
